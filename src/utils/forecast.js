7c3a38619ded0d7fe317a0b9ad7ce7c6b55acc9b
PravallikaPractice <pravallika.practice@gmail.com>
2020-09-30 14:13:36 +0530

    Removed Commented code

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,31 +1,5 @@
 const request = require('request')
 
-/*
-const url = 'http://api.weatherstack.com/current?access_key=6399053636e692675caa0c31b92fcf84&query=40.714,-74.006'
-request({url:url, json :true},(error,response)=>{
-    if(error){
-        return console.log("Unable to connect to Weather Service")
-    }else if(response.body.error){
-        return console.log("Unable to find the location")
-    }
-    console.log('It is currently ' , response.body.current.temperature ,'degress out. It is ',response.body.current.weather_descriptions[0])
-})*/
-/*
-const forecast = (latitude,longitude,callback)=>{
-    const forecasturl = 'http://api.weatherstack.com/current?access_key=6399053636e692675caa0c31b92fcf84&query='+encodeURIComponent(latitude)+','+encodeURIComponent(longitude)
-    request({url:forecasturl,json:true},(error,response)=>{
-        if(error){
-            callback("Unable to connect to Weather Service")
-        }
-        else if(response.body.error){
-            callback("Unable to find the location")
-        }
-        else{
-            callback(undefined,response.body.current)
-        }
-    })
-}
-*/
 //Code modifications using Object Destructuring and PropertyShorthand 
 const forecast = (latitude,longitude,callback)=>{
     const url = 'http://api.weatherstack.com/current?access_key=6399053636e692675caa0c31b92fcf84&query='+latitude+','+longitude
@@ -41,6 +15,5 @@ const forecast = (latitude,longitude,callback)=>{
         }
     })
 }
-
 
 module.exports = forecast
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,27 +1,5 @@
 const request = require('request')
-/*
-const geoCode = (address,callback)=>{
-    const geoCodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/+'+ encodeURIComponent(address)+'.json?access_token=pk.eyJ1IjoicHJhdmFsbGlrYWJhdGhhbGEiLCJhIjoiY2tmbnAwbjA3MDE0bTJ4bzA2Y2Fna2RmbSJ9.4Cez2DPoHbw0pFsmi2n5Mw&limit=1'
-    request({url:geoCodeUrl,json :true},(error,response)=>{
-        
-        if(error){
-            callback("Unable to connect to GeoLocation Service")
-        }
-        else if(response.body.error){
-            callback("Unable to find the Geo location")
-        }
-        else if(response.body.features.length === 0){
-            callback("Unable to find the Geo location")
-        }else{
-            callback(undefined,{
-                latitude : response.body.features[0].center[0],
-                longitude : response.body.features[0].center[1],
-                place : response.body.features[0].place_name
-            })
-        }
-    })
-}
-*/
+
 //Code modifications using Object Destructuring and PropertyShorthand 
 const geocode = (address,callback)=>{
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address)+'.json?access_token=pk.eyJ1IjoicHJhdmFsbGlrYWJhdGhhbGEiLCJhIjoiY2tmbnAwbjA3MDE0bTJ4bzA2Y2Fna2RmbSJ9.4Cez2DPoHbw0pFsmi2n5Mw&limit=1'
